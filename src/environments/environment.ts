e8841de227582d689c3b85666afbc0b63e4cb3f1
Anatolii <bugrovka12@ukr.net>
2019-05-03 19:10:03 +0300

    Fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  mapsApiKey: 'AIzaSyBQECxf09-gVALpKjsoHz-B_eRNmNs4jTA'
+  production: false
 };
 
 /*
